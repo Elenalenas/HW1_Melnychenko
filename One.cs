b197cbc7d97e166eb83c4d00036d7a107bd87016
Lena <elenameln25033@gmail.com>
2020-10-13 15:39:49 +0300

    Add project files.

---
+++ One.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW1_Melnychenko
+{
+    public class One
+    {
+        public string Name = "Lena";
+        public string Surname = "Melnychenko";
+        public int Age = 28;
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HW1_Melnychenko
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            One one = new One();
+            Console.WriteLine("Name: " + one.Name);
+            Console.WriteLine("Surname: " + one.Surname);
+            Console.WriteLine("Age: " + one.Age);
+            Console.Write("Profession: ");
+            string profession = Console.ReadLine();
+            Console.WriteLine("Nice Guy");
+            Console.ReadLine();
+        }
+    }
+}
